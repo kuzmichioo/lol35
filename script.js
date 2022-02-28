948d9aa14c6546b1b328b28c1ca0a267cbe9ddb4
kuzmichioo <kuzmichiooo@inbox.ru>
2022-02-28 20:43:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ fetch('https://api.github.com/users/kuzmichioo')
 .then(res => res.json())
 .then(json => console.log(json))
 .catch(err => console.log(err));
+
+let url = window.location.toString();
+
+function checkUsername(url) {
+  let urlSplit = url.split('=');
+  let name - urlSplit[1];
+  if (name == undefined) {
+    name = 'kuzmichioo';
+  }
+  else {
+    return name;
+  }
+}
+console.log(checkUsername(url));
+
+fetch('https://api.github.com/users/${checkUsername(url)}')
+  .then(res => res.json())
+  .then(json => console.log(json))
