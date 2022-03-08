a8f80954517da8601932f10321243e401e2493af
kuzmichioo <kuzmichiooo@inbox.ru>
2022-03-08 14:08:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let url = window.location.toString();
 
 function checkUsername(url) {
   let urlSplit = url.split('=');
-  let name - urlSplit[1];
+  let name = urlSplit[1];
   if (name == undefined) {
     name = 'kuzmichioo';
   }
@@ -17,6 +17,26 @@ function checkUsername(url) {
 }
 console.log(checkUsername(url));
 
-fetch('https://api.github.com/users/${checkUsername(url)}')
-  .then(res => res.json())
-  .then(json => console.log(json))
+fetch('https://api.github.com/users/${checkUsername(url))')
+  .then((res) => res.json())
+  .then((json) => {
+    let nameLink = document.createElement("fc");
+    nameLink.id = "link";
+    nameLink.href = json.html_url;
+    nameLink.title = "Link";
+    nameLink.innerHTML = json.name;
+    document.body.appenChild(nameLink);
+
+    if (!json.name) {
+      nameLink.innerHTML = "kuzmichioo";
+    }
+
+    let divBio = document.createElement("div");
+    divBio.id = "bio";
+    if (json.bio) {
+      divBio.innerHTML = json.bio;
+    }
+    else {
+      divBio.innerHTML = "ho ho ho"
+    }
+  }
